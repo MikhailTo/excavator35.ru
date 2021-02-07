bc8a16d1d34e5e3cbf5222b7d93d2f77ff29d59c
MikhailTo <toshkin.mikhail@yandex.ru>
2021-02-07 22:23:38 +0300

    Revert "restyle feedback, media"
    This reverts commit 6cac153b86b5d824ca678d3d18cbb6558fba6bf6.

--- partials/feedback.php
+++ partials/feedback.php
@@ -1,7 +1,7 @@
 <section id="feedback" class="feedback page-section">
     <div class="caption">
         <h3 class="caption__title">
-            Связаться с нами
+            Оставьте заявку
         </h3>
         <span class="line line--short"></span>
         <p class="caption__subtitle">Оставьте заявку и мы с Вами свяжемся!</p>
@@ -10,54 +10,54 @@
     <form id="feedback-form" class="form" action="<?php bloginfo('template_directory'); ?>/apps/feedback/process/process.php"
         enctype="multipart/form-data" novalidate>
 
-        <div class="form__body">
+        <div class="form-body">
 
-            <div class="form__col">
+            <div class="form-col">
                 <!-- Имя пользователя -->
-                <div class="form__group">
-                    <label for="name" class="control__label">Имя / Компания </label>
-                    <input id="name" type="text" name="name" class="form__control" value="" minlength="2" maxlength="30"
+                <div class="form-group">
+                    <label for="name" class="control-label">Имя / Компания </label>
+                    <input id="name" type="text" name="name" class="form-control" value="" minlength="2" maxlength="30"
                         required="required" placeholder="Введите имя или наименование компании">
                     <div class="invalid-feedback"></div>
                 </div>
 
                 <!-- Email пользователя -->
-                <div class="form__group">
-                    <label for="email" class="control__label">Email-адрес</label>
-                    <input id="email" type="email" name="email" required="required" class="form__control" value="" placeholder="Введите Email-адрес">
+                <div class="form-group">
+                    <label for="email" class="control-label">Email-адрес</label>
+                    <input id="email" type="email" name="email" required="required" class="form-control" value="" placeholder="Введите Email-адрес">
                     <div class="invalid-feedback"></div>
                 </div>
 
                 <!-- Телефон пользователя -->
-                <div class="form__group">
-                    <label for="phone" class="control__label">Номер телефона</label>
-                    <input id="phone" type="tel" name="phone" class="form__control" required="required" placeholder="Введите номер телефона">
+                <div class="form-group">
+                    <label for="phone" class="control-label">Номер телефона</label>
+                    <input id="phone" type="tel" name="phone" class="form-control" required="required" placeholder="Введите номер телефона">
                     <div class="invalid-feedback"></div>
                 </div>
             </div>
 
-            <div class="form__col">
+            <div class="form-col">
                 <!-- Сообщение пользователя -->
-                <div class="form__group">
-                    <label for="message" class="control__label">Сообщение</label>
-                    <textarea id="message" name="message" class="form__control"
+                <div class="form-group">
+                    <label for="message" class="control-label">Сообщение</label>
+                    <textarea id="message" name="message" class="form-control"
                         placeholder="Введите сообщение (не менее 20 символов)" rows="3" minlength="20" maxlength="500"
                         required="required"></textarea>
                     <div class="invalid-feedback"></div>
                 </div>
 
                 <!-- Капча -->
-                <div class="form__group form-captcha">
+                <div class="form-group form-captcha">
 
                     <img class=" form-captcha__refresh form-captcha__image"
                         src="<?php bloginfo('template_directory'); ?>/apps/feedback/captcha/captcha.php"
                         data-src="<?php bloginfo('template_directory'); ?>/apps/feedback/captcha/captcha.php"
                         width="132" height="46" alt="Капча">
 
-                    <div class="form__group form-captcha__input">
-                        <label for="captcha" class="control__label">Код, показанный на изображении</label>
+                    <div class="form-group form-captcha__input">
+                        <label for="captcha" class="control-label">Код, показанный на изображении</label>
                         <input type="text" name="captcha" maxlength="6" required="required" id="captcha"
-                            class="form__control captcha" placeholder="******" autocomplete="off" value="">
+                            class="form-control captcha" placeholder="******" autocomplete="off" value="">
                         <div class="invalid-feedback"></div>
                     </div>
                 </div>
@@ -65,10 +65,10 @@
         </div>
 
 
-        <div class="form__footer">
+        <div class="form-footer">
 
             <!-- Сообщение при ошибке -->
-            <div class="form__error d-none">
+            <div class="form-error d-none">
                 Исправьте данные и отправьте форму ещё раз.
             </div>
 
@@ -79,14 +79,14 @@
             </div>
 
             <!-- Кнопка для отправки формы на сервер -->
-            <div class="form__submit">
+            <div class="form-submit">
                 <button class="button" type="submit">Отправить</button>
             </div>
         </div>
     </form>
 
     <!-- Сообщение об успешной отправки формы -->
-    <div class="form__result-success d-none">
+    <div class="form-result-success d-none">
         <div>Форма успешно отправлена. Нажмите на <a href="#" data-target="#feedback-form">ссылку</a>, чтобы отправить
             ещё одно сообщение.</div>
     </div>
