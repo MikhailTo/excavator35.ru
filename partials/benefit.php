6aef0c1e83e49f91f382a7a26eb3d954847cc074
MikhailTo <toshkin.mikhail@yandex.ru>
2021-02-07 18:06:13 +0300

    corrected intro and rewrite styles of benefits in bem, media

--- partials/benefit.php
+++ partials/benefit.php
@@ -8,21 +8,21 @@
 			предназначенной для разных видов строительных работ.
 			У нас доступные цены на все виды услуг.</p>
 	</div>
-	<div class="benefit-items">
-		<div class="benefit-item">
-			<img class="benefit-icon" alt="pro" src="<?php bloginfo('template_directory'); ?>/assets/dist/svg/good.svg"></img>
-			<h2 class="benefit-title">Опытные операторы</h2>
-			<p class="benefit-subtitle">Машинисты профессионалы сделают работу на самом высоком уровне</p>
+	<div class="benefit__items">
+		<div class="benefit__item">
+			<img class="benefit__icon" alt="pro" src="<?php bloginfo('template_directory'); ?>/assets/dist/svg/good.svg"></img>
+			<h2 class="benefit__title">Опытные операторы</h2>
+			<p class="benefit__subtitle">Машинисты профессионалы сделают работу на самом высоком уровне</p>
 		</div>
-		<div class="benefit-item">
-			<img class="benefit-icon" alt="time" src="<?php bloginfo('template_directory'); ?>/assets/dist/svg/clock.svg"></img>
-			<h2 class="benefit-title">Работаем круглосуточно</h2>
-			<p class="benefit-subtitle">Возможна работа в две смены и без выходных</p>
+		<div class="benefit__item">
+			<img class="benefit__icon" alt="time" src="<?php bloginfo('template_directory'); ?>/assets/dist/svg/clock.svg"></img>
+			<h2 class="benefit__title">Работаем круглосуточно</h2>
+			<p class="benefit__subtitle">Возможна работа в две смены и без выходных</p>
 		</div>
-		<div class="benefit-item">
-			<img class="benefit-icon" alt="work" src="<?php bloginfo('template_directory'); ?>/assets/dist/svg/multitask.svg"></img>
-			<h2 class="benefit-title">Все виды работ</h2>
-			<p class="benefit-subtitle">Котлованы, рекультивация, благоустройство, демонтаж и прочее.</p>
+		<div class="benefit__item">
+			<img class="benefit__icon" alt="work" src="<?php bloginfo('template_directory'); ?>/assets/dist/svg/multitask.svg"></img>
+			<h2 class="benefit__title">Все виды работ</h2>
+			<p class="benefit__subtitle">Котлованы, рекультивация, благоустройство, демонтаж и прочее.</p>
 		</div>
 	</div>
 </section>
