90120262bd66d1060208a2add4b44a5968594643
MikhailTo <toshkin.mikhail@yandex.ru>
2021-02-07 15:58:38 +0300

    corrected intro section, included caption component

--- partials/intro.php
+++ partials/intro.php
@@ -1,28 +1,18 @@
 <section id="intro" class="intro page-section">
-	<div class="intro-inner">
-		<div class="intro-text">
-			<h2 class="intro-title">+7 921 530-61-77</h2>
+	<div class="intro__inner">
+
+		<div class="caption caption--intro">
+			<h3 class="caption-title caption-title--intro">
+				+7 921 530-61-77
+			</h3>
 			<span class="line"></span>
-			<h2 class="intro-subtitle">Аренда спецтехники в Вологде</h2>
+			<p class="caption-subtitle caption-subtitle--intro">
+				Аренда спецтехники <br> в Вологде
+			</p>
 		</div>
-		<div class="intro-action">
+
+		<div class="intro__action">
 			<button onclick="document.location='#feedback'" class="button">Сделать заявку</button>
 		</div>
-		<!--<div class="intro-form">
-			<form class="form" action="post">
-				<div class="form-header">
-					<h2 class="form-caption">Оставьте заявку</h2>
-				</div>
-				<div class="form-body">
-					<input type="text" id="name" placeholder="Ваше имя / Компания">
-					<input type="email" id="email" placeholder="Ваш e-mail">
-					<input type="tel" id="tel" placeholder="Ваш телефон">
-					<textarea id="message" placeholder="Ваше сообщение"></textarea>
-				</div>
-				<div class="form-footer">
-					<button class="button button--intro-form">Отправить</button>
-				</div>
-			</form>
-		</div>-->
 	</div>
 </section>
